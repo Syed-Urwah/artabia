9a896cd1ec2cd92d55684c66d3f9773aedd701c1
Noor <noorehira300@gmail.com>
2024-01-10 15:23:50 +0500

    k

--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -3,25 +3,28 @@ import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 import { useRouter } from "next/router";
 import { useEffect, useLayoutEffect, useState } from "react";
-import { getPayload, getUserType, userIsAuthenticated } from "@/enviroment/auth";
+import {
+  getPayload,
+  getUserType,
+  userIsAuthenticated,
+} from "@/enviroment/auth";
 
 const Layout = ({ children }) => {
-
   const [userType, setUserType] = useState(false);
   const [userDetails, setUserDetails] = useState(null);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
 
-  useEffect(()=>{
-    console.log(getPayload())
+  useEffect(() => {
+    console.log(getPayload());
     if (userIsAuthenticated()) {
       const payload = getPayload();
       setUserType(payload.user_type);
-      console.log(payload.user_type)
+      console.log(payload.user_type);
       setUserDetails(payload);
 
-      if(payload.user_type !== 'user'){
-        router.push('/artist/artist_dashboard')
+      if (payload.user_type !== "user") {
+        router.push("/artist/artist_dashboard");
       }
     } else {
       const payload = getPayload();
@@ -29,15 +32,21 @@ const Layout = ({ children }) => {
         router.push("/login");
         // return;
       }
-
-      
     }
-  },[])
 
-  return (
-    <div>
-      
-      <Header userType={userType} setUserType={setUserType} userDetails={userDetails} setUserDetails={setUserDetails} />
+    setLoading(false);
+  }, []);
+
+  return loading ? (
+    "loading"
+  ) : (
+    <div className="overflow-x-hidden">
+      <Header
+        userType={userType}
+        setUserType={setUserType}
+        userDetails={userDetails}
+        setUserDetails={setUserDetails}
+      />
       {children}
       <Footer />
     </div>
